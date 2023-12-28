71f3c6ce4ee7444306942bf3364fc74556d9c57d
karim.cheurfi <karim@revolve.team>
2023-12-28 15:43:14 +0100

    wekwewwkjekwswekwejwejwkwjekjw

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-ci-cd-handson-revolve-2"
+    bucket = "jenkins-ci-cd-revolve-2"
     key    = "handons/api-gateway/terraform.tfstates"
     region = "eu-west-1"
   }
